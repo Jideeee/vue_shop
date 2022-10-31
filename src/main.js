5adba8007ce016ca3c311d02b738b3519eec3621
Jideeee <a15361472569@163.com>
2022-10-31 13:32:43 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+import '@/assets/css/global.css'
+import axios from 'axios'
+Vue.prototype.$http =  axios
+//配置请求的根路径
+axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
